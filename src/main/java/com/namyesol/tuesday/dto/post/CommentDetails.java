dfd10b66c6d168cdda830d646a3de5ce1146eaa3
namyesol <studhaza@naver.com>
2023-09-10 00:57:13 +0900

    [#7] Response DTO 추가
    상세 페이지 응답용   DTO를 추가하였습니다.
    - PostDetails
    - CommentDetails

---
+++ src/main/java/com/namyesol/tuesday/dto/post/CommentDetails.java
@@ -0,0 +1,74 @@
+package com.namyesol.tuesday.dto.post;
+
+import java.time.LocalDateTime;
+
+public class CommentDetails {
+    private Long id;
+    private Long authorId;
+    private String authorName;
+    private Long postId;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+	
+    public CommentDetails() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getAuthorId() {
+		return authorId;
+	}
+
+	public void setAuthorId(Long authorId) {
+		this.authorId = authorId;
+	}
+
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+
+	public Long getPostId() {
+		return postId;
+	}
+
+	public void setPostId(Long postId) {
+		this.postId = postId;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+}
---
+++ src/main/java/com/namyesol/tuesday/dto/post/PostDetails.java
@@ -0,0 +1,82 @@
+package com.namyesol.tuesday.dto.post;
+
+import java.time.LocalDateTime;
+
+public class PostDetails {
+    private Long id;
+    private Long authorId;
+    private String authorName;
+    private String title;
+    private String content;
+    private Integer views;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public PostDetails() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Long authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getViews() {
+        return views;
+    }
+
+    public void setViews(Integer views) {
+        this.views = views;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+}
